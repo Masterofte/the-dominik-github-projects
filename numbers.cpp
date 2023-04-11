5d313c64a220170f693d402ab12e01641567450d
Dominik Stanczak <dominik.stanczak@tuhh.de>
2023-04-11 12:27:51 +0200

    changed some issues with debugging

--- numbers.cpp
+++ numbers.cpp
@@ -1,6 +1,14 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <cmath>
 
 int main() {
     return 0; //this is a comment test
+}
+*/
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
